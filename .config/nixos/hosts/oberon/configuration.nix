f3313a842600fe52c4d2c672eada3edd466ae820
Antoine Honinckx <65335212+antoine-hnkx@users.noreply.github.com>
2024-01-27 11:29:52 +0100

    add secondary ntfs drive in oberon nixos config

--- .config/nixos/hosts/oberon/configuration.nix
+++ .config/nixos/hosts/oberon/configuration.nix
@@ -1,8 +1,17 @@
 # Oberon specific configuration
 
-{ nixpkgs, ... }:
+{ nixpkgs, pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
   networking.hostName = "oberon";
+
+  environment.systemPackages = with pkgs; [
+    ntfs3g
+  ];
+  boot.supportedFilesystems = [ "ntfs" ];
+  # 1. mount ntfs drive
+  # ---- sudo mount /dev/sda1 /mnt/Data -t ntfs3
+  # 2. regenerate hardware-config.nix
+  # ---- nixos-generate-config --show-hardware-config > /home/$USER/.config/nixos/hosts/$HOST/hardware-configuration.nix
 }
