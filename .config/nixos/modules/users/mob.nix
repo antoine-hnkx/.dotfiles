b7455a27e29d1c7354c8fddabfaf302b9c3bf9fe
Antoine Honinckx <65335212+antoine-hnkx@users.noreply.github.com>
2024-02-18 15:44:24 +0100

    add nix support in vscode + add extensions in nixos config

--- .config/nixos/modules/users/mob.nix
+++ .config/nixos/modules/users/mob.nix
@@ -23,7 +23,13 @@
       # Programming related
       (vscode-with-extensions.override {
         vscodeExtensions = with vscode-extensions; [
-          bbenoist.nix
+          amodio.toggle-excluded-files
+          drcika.apc-extension
+          enkia.tokyo-night
+          esbenp.prettier-vscode
+          jnoortheen.nix-ide
+          mhutchie.git-graph
+          PKief.material-icon-theme
         ];
       })
       neovim
@@ -32,6 +38,7 @@
       beekeeper-studio
 
       # Programming environments
+      nixpkgs-fmt
       nodejs
       nodePackages.pnpm
       jdk21
